7c2eb9372948efe26f2e4eb28c221ae9c3a34a50
kkt12121 <kkt34343@gmail.com>
2025-01-09 22:47:44 +0900

    nestjs 연습

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  // NotFoundException,
+  Param,
+  // Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { PostsService } from './posts.service';
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  getPosts() {
+    return this.postsService.getAllposts();
+  }
+
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    return this.postsService.getPostById(+id);
+  }
+
+  @Post()
+  postPosts(
+    @Body('author') author: string,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ) {
+    return this.postsService.createPost(author, title, content);
+  }
+
+  @Put(':id')
+  putPosts(
+    @Param('id') id: string,
+    @Body('author') author?: string,
+    @Body('title') title?: string,
+    @Body('content') content?: string,
+  ) {
+    return this.postsService.updatePost(+id, author, title, content);
+  }
+
+  @Delete(':id')
+  deletePosts(@Param('id') id: string) {
+    return this.postsService.deletePost(+id);
+  }
+}
